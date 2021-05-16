24b154a02d1dbde20c1dda0db554a9e3967a09c4
henriqueeyer <henriqueeyer@outlook.com>
2021-05-16 13:19:07 -0300

    test: ensure button make a call and save token

--- src/__tests__/page-login.spec.js
+++ src/__tests__/page-login.spec.js
@@ -1,14 +1,60 @@
 import React from 'react';
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import fetchMock from 'fetch-mock';
-import { Provider, CurrencyContext } from '../context'
-import Login from '../pages/login/login'
+import { Provider } from '../context'
 import { MemoryRouter } from "react-router-dom"
 import App from '../App';
+import 'jest-localstorage-mock';
+
+
+const bodyMock = {
+  time: {
+    updated: 'Mar 22, 2020 23:54:00 UTC',
+    updatedISO: '2020-03-22T23:54:00+00:00',
+    updateduk: 'Mar 22, 2020 at 23:54 GMT'
+  },
+  disclaimer: 'This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org',
+  bpi: {
+    USD: {
+      code: 'USD',
+      rate: '6,506.6717',
+      description: 'United States Dollar',
+      rate_float: 6506.6717
+    },
+    BTC: {
+      code: 'BTC',
+      rate: '1.0000',
+      description: 'Bitcoin',
+      rate_float: 1
+    },
+    BRL: {
+      code: 'BTC',
+      rate: '1.0000',
+      description: 'Bitcoin',
+      rate_float: 1
+    },
+    EUR: {
+      code: 'BTC',
+      rate: '1.0000',
+      description: 'Bitcoin',
+      rate_float: 1
+    },
+    CAD: {
+      code: 'BTC',
+      rate: '1.0000',
+      description: 'Bitcoin',
+      rate_float: 1
+    }
+  }
+}
 
 describe('Login Page', () => {
+  afterEach(() => {
+    fetchMock.restore()
+  })
+
   const renderLogin = () => {
-    const screen = render(
+    render(
       <Provider>
         <MemoryRouter initialEntries={['/login']}>
           <App />
@@ -40,8 +86,8 @@ describe('Login Page', () => {
     const iptPassword = getByLabelText('Senha')
     const btn = getByText('Entrar')
 
-    fireEvent.change(iptEmail, {target: {value: 'valid@email.com'}})
-    fireEvent.change(iptPassword,{ target: {value: '123456'}})
+    fireEvent.change(iptEmail, { target: { value: 'valid@email.com' } })
+    fireEvent.change(iptPassword, { target: { value: '123456' } })
     expect(btn).toBeInTheDocument()
     expect(btn.disabled).toBe(false)
   });
@@ -52,8 +98,8 @@ describe('Login Page', () => {
     const iptPassword = getByLabelText('Senha')
     const btn = getByText('Entrar')
 
-    fireEvent.change(iptEmail, {target: {value: 'invalid'}})
-    fireEvent.change(iptPassword,{ target: {value: '123456'}})
+    fireEvent.change(iptEmail, { target: { value: 'invalid' } })
+    fireEvent.change(iptPassword, { target: { value: '123456' } })
     expect(btn).toBeInTheDocument()
     expect(btn.disabled).toBe(true)
   });
@@ -64,9 +110,37 @@ describe('Login Page', () => {
     const iptPassword = getByLabelText('Senha')
     const btn = getByText('Entrar')
 
-    fireEvent.change(iptEmail, {target: {value: 'valid@email.com'}})
-    fireEvent.change(iptPassword,{ target: {value: 'invalid'}})
+    fireEvent.change(iptEmail, { target: { value: 'valid@email.com' } })
+    fireEvent.change(iptPassword, { target: { value: 'invalid' } })
     expect(btn).toBeInTheDocument()
     expect(btn.disabled).toBe(true)
   });
+
+  test('should button if click make a request', async () => {
+    fetchMock.mock('http://localhost:3001/api/login', {
+      body: { token: 'any_token' },
+      status: 200
+    })
+    fetchMock.mock('http://localhost:3001/api/crypto/btc', {
+      body: bodyMock,
+      status: 200
+    })
+    const getItemSpy = jest.spyOn(localStorage, 'setItem')
+
+    const { getByLabelText, getByText, queryByText } = renderLogin()
+    const iptEmail = getByLabelText('Email')
+    const iptPassword = getByLabelText('Senha')
+    const btn = getByText('Entrar')
+
+    fireEvent.change(iptEmail, { target: { value: 'valid@email.com' } })
+    fireEvent.change(iptPassword, { target: { value: '123456' } })
+    expect(btn).toBeInTheDocument()
+    expect(btn.disabled).toBe(false)
+    fireEvent.click(btn)
+    await expect(getByText('Carregando')).toBeInTheDocument()
+    await waitForElementToBeRemoved(getByText('Login'))
+    expect(queryByText('Login')).not.toBeInTheDocument()
+    expect(getItemSpy).toHaveBeenCalledWith('token', 'any_token')
+  });
 })
+
